99abd27b24c3242efbb2d5c864cfc21adc5e6d41
Nasasira Benjamin <bnasasira0@gmail.com>
2026-07-20 14:44:00 +0300

    info system displaying memory usage and disk usage

---
+++ week2/system_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+current_user=$(whoami)
+echo "The $current_user is: $current_user"
+current_dir=$(pwd)
+echo "Your in this folder: $current_dir"
+current_date=$(date)
+echo "Today is $current_date"
+
+disk_space=$(df-h)
+echo "The disk used is: $disk_space"
+
+memory_usage=$(free-h)
+echo "The memory used is: $memory_usage"
+
+
