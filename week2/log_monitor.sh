096491df825eab316e6293a454667c492bc73558
Nasasira Benjamin <bnasasira0@gmail.com>
2026-07-21 10:54:54 +0300

    logging monitor works perfectly

--- week2/log_monitor.sh
+++ week2/log_monitor.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-mkdir -p logs
-timestamp=$(date)
-echo "Script exected at $timestamp"
-tail -n 5 logs
-wc -l < logs
 
+mkdir -p logs
+
+log_file="logs/activity.log"
+timestamp=$(date)
+
+echo "Script executed at $timestamp" >> $log_file
+
+echo "Last 5 entries:"
+tail -n 5 $log_file
+
+echo "Total times script has run:"
+wc -l < $log_file
